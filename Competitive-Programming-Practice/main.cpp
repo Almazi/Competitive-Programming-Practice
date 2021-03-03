b690368aad2ed1fb98fc7803300b441d8d39dc87
Almazi <navilalmazi@gmail.com>
2021-03-02 18:48:22 -0700

    Initial Commit

---
+++ Competitive-Programming-Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Competitive-Programming-Practice
+//
+//  Created by Ahmad Almazi on 2/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
